6eccf05ffbd7013ff766b6b6bdd36db3382f7d45
Lucas Nakano <lucas_nakano@hotmail.com>
2021-11-08 10:25:45 -0300

    Pod e Config

---
+++ Pods/Target Support Files/Pods-MarvelsCharacters-MarvelsCharactersUITests/Pods-MarvelsCharacters-MarvelsCharactersUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MarvelsCharacters_MarvelsCharactersUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MarvelsCharacters_MarvelsCharactersUITests
+@end
---
+++ Pods/Target Support Files/Pods-MarvelsCharacters/Pods-MarvelsCharacters-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MarvelsCharacters : NSObject
+@end
+@implementation PodsDummy_Pods_MarvelsCharacters
+@end
