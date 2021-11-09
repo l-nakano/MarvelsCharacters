c839a3794137bbdbeec7c7eef20bef1482493fca
Lucas Nakano <lucas_nakano@hotmail.com>
2021-11-09 11:48:32 -0300

    PageControl inserido + Navegação entre listas

--- MarvelsCharacters/ViewController.swift
+++ MarvelsCharacters/ViewController.swift
@@ -10,23 +10,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var personagem3Label: UILabel!
     @IBOutlet weak var personagem4ImageView: UIImageView!
     @IBOutlet weak var personagem4Label: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var previousPageButton: UIButton!
+    @IBOutlet weak var nextPageButton: UIButton!
     
     let charactersRepository = CharactersRepository()
+    var charactersList: [Character] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         charactersRepository.getCharacterList { characters in
-            self.personagem1ImageView.image = UIImage(systemName: "photo")
-            self.personagem1Label.text = characters[0].name
-            self.personagem2ImageView.image = UIImage(systemName: "photo")
-            self.personagem2Label.text = characters[1].name
-            self.personagem3ImageView.image = UIImage(systemName: "photo")
-            self.personagem3Label.text = characters[2].name
-            self.personagem4ImageView.image = UIImage(systemName: "photo")
-            self.personagem4Label.text = characters[3].name
+            self.charactersList = characters
+            self.pageControl.numberOfPages = characters.count / 4
+            self.updateView()
         }
     }
-
+    
+    func updateView() {
+        self.personagem1ImageView.image = UIImage(systemName: "photo")
+        self.personagem1Label.text = charactersList[pageControl.currentPage * 4].name
+        self.personagem2ImageView.image = UIImage(systemName: "photo")
+        self.personagem2Label.text = charactersList[pageControl.currentPage * 4 + 1].name
+        self.personagem3ImageView.image = UIImage(systemName: "photo")
+        self.personagem3Label.text = charactersList[pageControl.currentPage * 4 + 2].name
+        self.personagem4ImageView.image = UIImage(systemName: "photo")
+        self.personagem4Label.text = charactersList[pageControl.currentPage * 4 + 3].name
+        
+        switch pageControl.currentPage {
+        case 0:
+            previousPageButton.alpha = 0
+        case pageControl.numberOfPages - 1:
+            nextPageButton.alpha = 0
+        default:
+            previousPageButton.alpha = 1
+            nextPageButton.alpha = 1
+        }
+    }
+    
+    @IBAction func previousPageButtonPressed(_ sender: Any) {
+        pageControl.currentPage -= 1
+        updateView()
+    }
+    
+    @IBAction func nextPageButtonPressed(_ sender: Any) {
+        pageControl.currentPage += 1
+        updateView()
+    }
+    
 }
 
